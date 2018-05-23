a6cbb84b8a956282e04ed86d7224b59d4aedcfa0
Michael Ferguson <meister@spacekace.com>
2018-05-23 09:29:17 -0700

    allow the default to be configurable

--- app/helpers/edison/application_helper.rb
+++ app/helpers/edison/application_helper.rb
@@ -1,25 +1,29 @@
 module Edison
 	module ApplicationHelper
 
-		def edison_run_experiment( exp_id )
+		def edison_run_experiment( exp_id, options = {} )
+			options[:default] = '' unless options.has_key? :default
 
 			begin
 				experiment = Experiment.friendly.find( exp_id )
 			rescue ActiveRecord::RecordNotFound
-				return ''
+				return options[:default]
 			end
 
 			if experiment.started? && experiment.concluded?
+
 				# the music's over
 				if experiment.conclusion_type == 'control'
 					return experiment.variants.where( is_control: true ).last.content
 				elsif experiment.conclusion_type == 'winner'
 					return experiment.variants.order( cached_conversion_count: :desc ).first.content
 				else
-					return ''
+					return options[:default]
 				end
+
 			elsif not( experiment.active? ) || not( experiment.started? )
-				return ''
+				# the music hasn't started yet
+				return options[:default]
 			end
 
 
